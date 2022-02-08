7e189924c26edb500adadf9290e49cb4fd0fb803
jasonmsetiadi <66339366+jasonmsetiadi@users.noreply.github.com>
2022-02-07 18:05:03 -0600

    test

--- scope/fritz.py
+++ scope/fritz.py
@@ -1,43 +1,54 @@
 import pandas as pd
 
-def get_highscoring_objects(otype='vnv',condition="$or",
-    limit=0.9,limit_dnn=None,limit_xgb=None,):
 
-    if limit_dnn == None:
+def get_highscoring_objects(
+    G,
+    otype='vnv',
+    condition="$or",
+    limit=0.9,
+    limit_dnn=None,
+    limit_xgb=None,
+):
+
+    if limit_dnn is None:
         limit_dnn = limit
-    if limit_xgb == None:
+    if limit_xgb is None:
         limit_xgb = limit
 
-    ### example
-    q = {"query_type": "find",
-             "query": {
-                 "catalog": "ZTF_source_classifications_DR5",
-                 "filter": { condition : [{'%s_xgb' %otype: { '$gt': limit_xgb }},
-                                    {'%s_dnn' %otype: { '$gt': limit_dnn }}],
-                           },
-                 "projection": {}
-             },
-             "kwargs": {     }
-             }
+    # example
+    q = {
+        "query_type": "find",
+        "query": {
+            "catalog": "ZTF_source_classifications_DR5",
+            "filter": {
+                condition: [
+                    {'%s_xgb' % otype: {'$gt': limit_xgb}},
+                    {'%s_dnn' % otype: {'$gt': limit_dnn}},
+                ],
+            },
+            "projection": {},
+        },
+        "kwargs": {},
+    }
 
     r = G.query(q)
 
     return pd.DataFrame(r['data'])
 
-def get_stats(ids):
+
+def get_stats(G, ids):
     qs = [
-           {"query_type": "find",
-             "query": {
-                 "catalog": "ZTF_source_features_DR5",
-                 "filter": {'_id': i
-                            },
-                 "projection": {}
-             },
-             "kwargs": {     }
-             }
+        {
+            "query_type": "find",
+            "query": {
+                "catalog": "ZTF_source_features_DR5",
+                "filter": {'_id': i},
+                "projection": {},
+            },
+            "kwargs": {},
+        }
         for i in ids
     ]
     rs = G.batch_query(qs, n_treads=32)
 
     return pd.DataFrame([s['data'][0] for s in rs])
-
--- scope_jason.py
+++ scope_jason.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import pdb
-import json
 from penquins import Kowalski
 import numpy as np
 from scope.fritz import get_highscoring_objects, get_stats
 import argparse
 import csv
 
-## load dataset
+# load dataset
 parser = argparse.ArgumentParser()
 parser.add_argument("inputfile")
 parser.add_argument("--id", type=int, default=1, help="group id on Fritz")
@@ -19,23 +17,28 @@ with open(args.inputfile) as f:
 # Kowalski
 with open('password.txt', 'r') as f:
     password = f.read().splitlines()
-G = Kowalski(username=password[0], password=password[1], host='gloria.caltech.edu', timeout=1000)
+G = Kowalski(
+    username=password[0], password=password[1], host='gloria.caltech.edu', timeout=1000
+)
 
 # get scores and data and combine
-scores = get_highscoring_objects(otype='vnv',condition='$or')
+scores = get_highscoring_objects(G, otype='vnv', condition='$or')
 
 index = scores.index
-condition = ((scores["vnv_dnn"]>0.95)&(scores['vnv_xgb']<=0.1)) | ((scores["vnv_dnn"]<=0.1)&(scores['vnv_xgb']>0.95))
+condition = ((scores["vnv_dnn"] > 0.95) & (scores['vnv_xgb'] <= 0.1)) | (
+    (scores["vnv_dnn"] <= 0.1) & (scores['vnv_xgb'] > 0.95)
+)
 disagreements = index[condition]
-disagreeing_scores = scores.iloc[disagreements,:]
+disagreeing_scores = scores.iloc[disagreements, :]
 
-stats = get_stats(disagreeing_scores['_id'])
-data = pd.merge(disagreeing_scores,stats,left_on='_id',right_on='_id')
-data['train'] = np.isin(data['_id'],trainingset['ztf_id'])
+stats = get_stats(G, disagreeing_scores['_id'])
+data = pd.merge(disagreeing_scores, stats, left_on='_id', right_on='_id')
+data['train'] = np.isin(data['_id'], trainingset['ztf_id'])
 sample = data[~data['train']]
 
 with open('token.txt', 'r') as f:
     token = f.read()
+
 
 def upload():
     import requests
@@ -45,43 +48,52 @@ def upload():
         response = requests.request(method, endpoint, json=data, headers=headers)
         return response
 
-
-    headers={"Authorization": f"token {token}"}
+    headers = {"Authorization": f"token {token}"}
 
     for index, row in sample.iterrows():
-        #print(row)
+        # print(row)
         i = row['_id']
-        period = row['period']
-        
-        # upload 
-        json = { "catalog": "ZTF_sources_20210401",
-                  "group_ids": [args.id]} 
+
+        # upload
+        json = {"catalog": "ZTF_sources_20210401", "group_ids": [args.id]}
         json['light_curve_ids'] = [int(i)]
         response = requests.post(
-          url='https://fritz.science/api/archive',
-          json=json,
-          headers=headers,)
+            url='https://fritz.science/api/archive',
+            json=json,
+            headers=headers,
+        )
 
         # get objid
         try:
             obj_id = response.json()['data']['obj_id']
-        except:
+        except Exception:
             continue
 
         # annotate
-        url = 'https://fritz.science/api/sources/%s/annotations' %obj_id
-        json = { "origin": "ML_DR5_d15_v1",
-              "group_ids": [args.id]}
-        
-        Gaia = {'Plx': row['Gaia_EDR3__parallax'],'Mag_G': row['Gaia_EDR3__phot_g_mean_mag'],'Mag_Bp': row['Gaia_EDR3__phot_bp_mean_mag'],'Mag_Rp': row['Gaia_EDR3__phot_rp_mean_mag']}
+        url = 'https://fritz.science/api/sources/%s/annotations' % obj_id
+        json = {"origin": "ML_DR5_d15_v1", "group_ids": [args.id]}
 
-        json['data'] = {'period': row['period'] , 
-                        'vnv': {'vnv_dnn': np.round(row['vnv_dnn'],3),  'vnv_xgb': np.round(row['vnv_xgb'],3)},
-                        'Gaia': Gaia}
+        Gaia = {
+            'Plx': row['Gaia_EDR3__parallax'],
+            'Mag_G': row['Gaia_EDR3__phot_g_mean_mag'],
+            'Mag_Bp': row['Gaia_EDR3__phot_bp_mean_mag'],
+            'Mag_Rp': row['Gaia_EDR3__phot_rp_mean_mag'],
+        }
+
+        json['data'] = {
+            'period': row['period'],
+            'vnv': {
+                'vnv_dnn': np.round(row['vnv_dnn'], 3),
+                'vnv_xgb': np.round(row['vnv_xgb'], 3),
+            },
+            'Gaia': Gaia,
+        }
 
         response = requests.post(
-          url=url,
-          json=json,
-          headers=headers,)
+            url=url,
+            json=json,
+            headers=headers,
+        )
+
 
 upload()
