9e68098c83d3617563586ecd5bcf87e6aafd9dc0
Ashish Mahabal <aam@astro.caltech.edu>
2022-04-20 21:50:42 -0700

    Improved docstrings, and added skip parameter

--- tools/get_quad_ids.py
+++ tools/get_quad_ids.py
@@ -2,22 +2,36 @@ from penquins import Kowalski
 import json
 
 
-def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, n=10):
+def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, skip=0, limit=10):
     '''Get ids for a specific quad of a CCD for a particular ZTF field.
+    Parameters
+    ----------
+    catalog : str
+        Catalog containing ids, CCD, quad, and light curves
+    field : int
+        ZTF field number
+    ccd : int
+        CCD number [1,16] (not checked)
+    quad : int
+        CCD quad number [1,4] (not checked)
+    minobs : int
+        Minimum points in the light curve for the object to be selected
+    skip : int
+        How many of the selected rows to skip
+        Along with limit this can be used to loop over a quad in chunks
+    limit : int
+        How many of the selected rows to return. Default is 10
+    Returns
+    -------
+    ids : list
+        A list of ids
 
-    USAGE: data = get_field_ids('ZTF_sources_20210401',field=301,ccd=2,quad=3,minobs=5,n=20)
-    It is assumed that you are connected to the database already.
-    ccd range: [1,16] (not checked)
-    quad range: p1,4] (not checked)
-    Defualt minobs is 5
-    Default number of ids to return is 10 (use 0 for all rows)
-    Output: list of ids
+    USAGE: data = get_field_ids('ZTF_sources_20210401',field=301,ccd=2,quad=3,\
+        minobs=5,skip=0,limit=20)
     '''
 
-    if n == 0:
+    if limit == 0:
         limit = 10000000000
-    else:
-        limit = n
 
     q = {
         'query_type': 'find',
@@ -33,7 +47,7 @@ def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, n=10):
                 "_id": 1,
             },
         },
-        "kwargs": {"limit": limit},
+        "kwargs": {"limit": limit, "skip": skip},
     }
 
     r = gloria.query(q)
@@ -48,5 +62,5 @@ if __name__ == "__main__":
     gloria = Kowalski(**secrets['gloria'], verbose=False)
 
     data = get_field_ids(
-        'ZTF_sources_20210401', field=301, ccd=2, quad=3, minobs=5, n=20
+        'ZTF_sources_20210401', field=301, ccd=2, quad=3, minobs=5, skip=0, limit=20
     )
