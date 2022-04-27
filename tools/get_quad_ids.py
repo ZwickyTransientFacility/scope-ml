0b184934dd70b418f8977abdb83ab3d13dc7ca05
Ashish Mahabal <aam@astro.caltech.edu>
2022-04-27 13:22:25 -0700

    argparse enabled including for token
    limit, skip, numobs etc. can now be passed as arguments

--- tools/get_quad_ids.py
+++ tools/get_quad_ids.py
@@ -1,3 +1,4 @@
+import argparse
 from penquins import Kowalski
 import json
 
@@ -56,11 +57,32 @@ def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, skip=0, limit=10)
 
 
 if __name__ == "__main__":
+
+    # pass Fritz token as command line argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-catalog", help="catalog")
+    parser.add_argument("-field", type=int, default=301, help="field number")
+    parser.add_argument("-ccd", type=int, default=4, help="ccd number")
+    parser.add_argument("-quad", type=int, default=3, help="quad number")
+    parser.add_argument(
+        "-minobs", type=int, default=20, help="minimum number of points in lc"
+    )
+    parser.add_argument("-skip", type=int, default=0, help="number of rows to skip")
+    parser.add_argument("-limit", type=int, default=10, help="number of rows to return")
+    parser.add_argument(
+        "-token",
+        type=str,
+        help="put your Fritz token here. You can get it from your Fritz profile page",
+    )
+    args = parser.parse_args()
+
     # setup connection to gloria to get the lightcurves
     with open('secrets.json', 'r') as f:
         secrets = json.load(f)
+    if args.token:
+        secrets['gloria']['token'] = args.token
     gloria = Kowalski(**secrets['gloria'], verbose=False)
 
-    data = get_field_ids(
-        'ZTF_sources_20210401', field=301, ccd=2, quad=3, minobs=5, skip=0, limit=20
-    )
+    #    data = get_field_ids(catalog='ZTF_sources_20210401',limit=args.limit)
+    data = get_field_ids(catalog=args.catalog, limit=args.limit)
+    print(data)
