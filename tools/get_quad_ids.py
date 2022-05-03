8a12ef534800b3df3cd2e6beee83f4d4b08275b5
Ashish Mahabal <aam@astro.caltech.edu>
2022-05-03 16:13:12 -0700

    Made output filename cumpulsory
    Also, token is optional if present in secrets file.
    It can be given as second argument, or with -token

--- tools/get_quad_ids.py
+++ tools/get_quad_ids.py
@@ -1,9 +1,11 @@
 import argparse
 from penquins import Kowalski
+import pandas as pd
 import json
+import sys
 
 
-def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, skip=0, limit=10):
+def get_field_ids(catalog, field=301, ccd=4, quad=3, minobs=20, skip=0, limit=10):
     '''Get ids for a specific quad of a CCD for a particular ZTF field.
     Parameters
     ----------
@@ -58,31 +60,56 @@ def get_field_ids(catalog, field=301, ccd=2, quad=3, minobs=5, skip=0, limit=10)
 
 if __name__ == "__main__":
 
-    # pass Fritz token as command line argument
+    # pass Fritz token through secrets.json or as a command line argument
+    with open('secrets.json', 'r') as f:
+        secrets = json.load(f)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-catalog", help="catalog")
-    parser.add_argument("-field", type=int, default=301, help="field number")
-    parser.add_argument("-ccd", type=int, default=4, help="ccd number")
-    parser.add_argument("-quad", type=int, default=3, help="quad number")
     parser.add_argument(
-        "-minobs", type=int, default=20, help="minimum number of points in lc"
+        "-catalog",
+        help="catalog (default: ZTF_sources_20210401)",
+        default='ZTF_sources_20210401',
     )
-    parser.add_argument("-skip", type=int, default=0, help="number of rows to skip")
-    parser.add_argument("-limit", type=int, default=10, help="number of rows to return")
     parser.add_argument(
-        "-token",
-        type=str,
-        help="put your Fritz token here. You can get it from your Fritz profile page",
+        "output",
+        action='store',
+        type=argparse.FileType('w'),
+        help="file to write output to",
     )
+
+    token_help = "put your Fritz token here or in the secrets file.\
+                    You can get it from your Fritz profile page. This becomes\
+                    an optional parameter if put in secrets file."
+    if 'token' not in secrets['gloria'] or secrets['gloria']['token'] == "":
+        if '-token' not in sys.argv:
+            parser.add_argument("token", type=str, help=token_help)
+    parser.add_argument("-token", type=str, help=token_help)
+    parser.add_argument(
+        "-field", type=int, default=301, help="field number (default 301)"
+    )
+    parser.add_argument("-ccd", type=int, default=4, help="ccd number (default 4)")
+    parser.add_argument("-quad", type=int, default=3, help="quad number (default 3)")
+    parser.add_argument(
+        "-minobs",
+        type=int,
+        default=20,
+        help="minimum number of points in lc (default 20)",
+    )
+    parser.add_argument(
+        "-skip", type=int, default=0, help="number of rows to skip (default 0)"
+    )
+    parser.add_argument(
+        "-limit", type=int, default=10, help="number of rows to return (default 10)"
+    )
+
     args = parser.parse_args()
 
     # setup connection to gloria to get the lightcurves
-    with open('secrets.json', 'r') as f:
-        secrets = json.load(f)
     if args.token:
         secrets['gloria']['token'] = args.token
     gloria = Kowalski(**secrets['gloria'], verbose=False)
 
     #    data = get_field_ids(catalog='ZTF_sources_20210401',limit=args.limit)
+    # print(data)
     data = get_field_ids(catalog=args.catalog, limit=args.limit)
-    print(data)
+    pd.DataFrame(data).to_csv(args.output, index=False, header=False)
