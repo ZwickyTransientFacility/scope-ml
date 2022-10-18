5100c38368a1218ca5ecac74b71c3323e64f0548
Brian Healy <42810347+bfhealy@users.noreply.github.com>
2022-10-18 17:09:13 -0500

    Add versioning (#132)
    This PR adds versioning to scope/__init__.py.

--- scope/__init__.py
+++ scope/__init__.py
@@ -1,2 +1,38 @@
 from .nn import *
 from .utils import *
+
+# Below code adapted from https://github.com/skyportal/skyportal/blob/main/skyportal/__init__.py
+# 10-18-2022
+__version__ = '0.2.dev0'
+
+if 'dev' in __version__:
+    # Append last commit date and hash to dev version information, if available
+
+    import subprocess
+    import os.path
+
+    try:
+        p = subprocess.Popen(
+            ['git', 'log', '-1', '--format="%h %aI"'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=os.path.dirname(__file__),
+        )
+    except FileNotFoundError:
+        pass
+    else:
+        out, err = p.communicate()
+        if p.returncode == 0:
+            git_hash, git_date = (
+                out.decode('utf-8')
+                .strip()
+                .replace('"', '')
+                .split('T')[0]
+                .replace('-', '')
+                .split()
+            )
+
+            __version__ = '+'.join(
+                [tag for tag in __version__.split('+') if not tag.startswith('git')]
+            )
+            __version__ += f'+git{git_date}.{git_hash}'
