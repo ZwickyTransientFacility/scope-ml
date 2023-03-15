c8b5e4bd4a7505b84c1712aa1c7cc150e795dd3f
Brian Healy <brian.francis.healy@gmail.com>
2023-03-15 11:15:47 -0500

    Revise pyaov build.sh

--- tools/featureGeneration/pyaov/build.sh
+++ tools/featureGeneration/pyaov/build.sh
@@ -1,2 +1,3 @@
-# Modify path as appopriate for your conda installer and environment name
-~/miniforge3/envs/scope-env/bin/f2py3 -m aov -c aovconst.f90 aovsub.f90 aov.f90
+# Move the file created by this script to your environment's site-packages directory
+# e.g. $HOME/miniforge3/envs/scope-env/lib/python3.10/site-packages/.
+python -m numpy.f2py -m aov -c aovconst.f90 aovsub.f90 aov.f90
