adc8f48d5f831b67332ee5b5bd63b20161bf58a4
Brian Healy <brian.francis.healy@gmail.com>
2023-04-21 10:47:45 -0500

    Save train/val/test plots

--- scope.py
+++ scope.py
@@ -751,13 +751,20 @@ class Scope:
 
         if verbose:
             print("Evaluating on test set:")
-        # Todo: there should not need to be this algorithm-based split in the call to classifier.evaluate()
+        # TODO: there should not need to be this algorithm-based split in the call to classifier.evaluate()
         if algorithm == 'xgb':
-            stats = classifier.evaluate(X_test, y_test)
+            stats_train = classifier.evaluate(X_train, y_train, name='train')
+            stats_val = classifier.evaluate(X_val, y_val, name='val')
+            stats_test = classifier.evaluate(X_test, y_test, name='test')
         else:
-            stats = classifier.evaluate(datasets["test"], verbose=verbose)
+            stats_train = classifier.evaluate(datasets["train"], verbose=verbose)
+            stats_val = classifier.evaluate(datasets["val"], verbose=verbose)
+            stats_test = classifier.evaluate(datasets["test"], verbose=verbose)
+
+        print('training stats: ', stats_train)
+        print('validation stats: ', stats_val)
         if verbose:
-            print(stats)
+            print('test stats: ', stats_test)
 
         if algorithm == 'DNN':
             param_names = (
@@ -773,7 +780,7 @@ class Scope:
             )
             if not kwargs.get("test", False):
                 # log model performance on the test set
-                for param, value in zip(param_names, stats):
+                for param, value in zip(param_names, stats_test):
                     wandb.run.summary[f"test_{param}"] = value
                 p, r = (
                     wandb.run.summary["test_precision"],
--- scope/xgb.py
+++ scope/xgb.py
@@ -67,23 +67,29 @@ class XGB(AbstractClassifier):
             **kwargs,
         )
 
-    def predict(self, X, **kwargs):
+    def predict(self, X, name=None, **kwargs):
         d = xgb.DMatrix(X)
         y_pred = self.model.predict(d)
-        self.meta['y_pred'] = y_pred
+
+        if name is not None:
+            self.meta[f'y_pred{name}'] = y_pred
+        else:
+            self.meta['y_pred'] = y_pred
 
         return y_pred
 
-    def evaluate(self, X_test, y_test, **kwargs):
-        dtest = xgb.DMatrix(X_test, label=y_test)
+    def evaluate(self, X_eval, y_eval, name='test', **kwargs):
+        d_eval = xgb.DMatrix(X_eval, label=y_eval)
 
-        y_pred = np.around(self.predict(X_test))
+        y_pred = np.around(self.predict(X_eval, name=f'_{name}'))
+
+        self.meta[f'X_{name}'] = X_eval
+        self.meta[f'y_{name}'] = y_eval
 
         # Generate confusion matrix
-        self.meta['cm'] = confusion_matrix(y_test, y_pred)
-        self.meta['y_test'] = y_test
+        self.meta[f'cm_{name}'] = confusion_matrix(y_eval, y_pred)
 
-        return self.model.eval(dtest, 'dtest', **kwargs)
+        return self.model.eval(d_eval, f'd{name}', **kwargs)
 
     def load(self, path_model, **kwargs):
         try:
@@ -98,66 +104,68 @@ class XGB(AbstractClassifier):
         output_path: str = "./",
         output_format: str = "json",
         plot: bool = False,
+        names: list = ['train', 'val', 'test'],
         **kwargs,
     ):
         if output_format not in ["json"]:
             raise ValueError("unknown output format")
 
-        path = pathlib.Path(output_path)
-        if not path.exists():
-            path.mkdir(parents=True, exist_ok=True)
+        output_path = pathlib.Path(output_path)
+        if not output_path.exists():
+            output_path.mkdir(parents=True, exist_ok=True)
 
         output_name = self.name if not tag else tag
         if not output_name.endswith('.json'):
             output_name += '.json'
-        self.model.save_model(path / output_name)
+        self.model.save_model(output_path / output_name)
 
         # Save diagnostic plots
-        if plot:
-            path = path / f"{tag}_plots"
-            if not path.exists():
-                path.mkdir(parents=True, exist_ok=True)
-            impvars = tag + '_impvars.pdf'
-            cmpdf = tag + '_cm.pdf'
-            recallpdf = tag + '_recall.pdf'
-            rocpdf = tag + '_roc.pdf'
+        for name in names:
+            if plot:
+                path = output_path / f"{tag}_plots" / name
+                if not path.exists():
+                    path.mkdir(parents=True, exist_ok=True)
+                impvars = tag + '_impvars.pdf'
+                cmpdf = tag + '_cm.pdf'
+                recallpdf = tag + '_recall.pdf'
+                rocpdf = tag + '_roc.pdf'
 
-            max_num_features = kwargs.get('max_num_features', 8)
+                max_num_features = kwargs.get('max_num_features', 8)
 
-            _ = xgb.plot_importance(
-                self.model, max_num_features=max_num_features, grid=False
-            )
-            plt.title(tag + ' Feature Importance')
-            plt.savefig(path / impvars, bbox_inches='tight')
-            plt.close()
-
-            if self.meta['cm'] is not None:
-                cname = tag.split('.')[0]
-                make_confusion_matrix(
-                    self.meta['cm'],
-                    figsize=(8, 6),
-                    cbar=False,
-                    percent=False,
-                    categories=['not ' + cname, cname],
+                _ = xgb.plot_importance(
+                    self.model, max_num_features=max_num_features, grid=False
                 )
-                sns.set_context('talk')
-                plt.title(cname)
-                plt.savefig(path / cmpdf, bbox_inches='tight')
+                plt.title(tag + ' Feature Importance')
+                plt.savefig(path / impvars, bbox_inches='tight')
                 plt.close()
 
-            y_test = self.meta.get('y_test', None)
-            y_pred = self.meta.get('y_pred', None)
+                if self.meta[f'cm_{name}'] is not None:
+                    cname = tag.split('.')[0]
+                    make_confusion_matrix(
+                        self.meta[f'cm_{name}'],
+                        figsize=(8, 6),
+                        cbar=False,
+                        percent=False,
+                        categories=['not ' + cname, cname],
+                    )
+                    sns.set_context('talk')
+                    plt.title(cname)
+                    plt.savefig(path / cmpdf, bbox_inches='tight')
+                    plt.close()
 
-            if (y_test is not None) & (y_pred is not None):
+                y_compare = self.meta.get(f'y_{name}', None)
+                y_pred = self.meta.get(f'y_pred_{name}', None)
 
-                fpr, tpr, _ = roc_curve(y_test, y_pred)
-                roc_auc = auc(fpr, tpr)
-                precision, recall, _ = precision_recall_curve(y_test, y_pred)
+                if (y_compare is not None) & (y_pred is not None):
 
-                plot_roc(fpr, tpr, roc_auc)
-                plt.savefig(path / rocpdf, bbox_inches='tight')
-                plt.close()
+                    fpr, tpr, _ = roc_curve(y_compare, y_pred)
+                    roc_auc = auc(fpr, tpr)
+                    precision, recall, _ = precision_recall_curve(y_compare, y_pred)
 
-                plot_pr(recall, precision)
-                plt.savefig(path / recallpdf, bbox_inches='tight')
-                plt.close()
+                    plot_roc(fpr, tpr, roc_auc)
+                    plt.savefig(path / rocpdf, bbox_inches='tight')
+                    plt.close()
+
+                    plot_pr(recall, precision)
+                    plt.savefig(path / recallpdf, bbox_inches='tight')
+                    plt.close()
